26db0db3ea18975474c92a12982009a630bd066b
Alexander Oster <Alexander.Oster@autodesk.com>
2020-09-22 16:34:52 +0200

    added cmath

--- Plugins/PrinterConnection/mcplugin_printerconnection.cpp
+++ Plugins/PrinterConnection/mcplugin_printerconnection.cpp
@@ -30,7 +30,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <chrono>
 #include <sstream>
-
+#include <cmath>
 
 #include "libmcplugin_statefactory.hpp"
 #include "libmcplugin_interfaceexception.hpp"
