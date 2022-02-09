aa9a9c7204eb5b76557e3ba84bb98c40c0a9d3f0
Alexander Oster <alexanderoster@users.noreply.github.com>
2022-02-09 15:24:33 +0100

    build script

--- Server/build_packageManager.bat
+++ Server/build_packageManager.bat
@@ -1,7 +1,7 @@
 @echo off
 
 echo "Building DownloadPackage..."
-go build -o ../build/DevPackage/packageManager.exe -ldflags="-s -w" ../Server/packageManager.go
+go build -o ../build_win64/DevPackage/packageManager.exe -ldflags="-s -w" ../Server/packageManager.go
 
 echo "done."
 
