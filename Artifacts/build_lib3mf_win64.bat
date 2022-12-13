eadf928143ee78b55f60dfeefb4e5e2b66cbe7fe
Alexander Oster <alexanderoster@users.noreply.github.com>
2022-12-13 12:22:47 +0100

    Updated Lib3MF

--- Artifacts/build_lib3mf_win64.bat
+++ Artifacts/build_lib3mf_win64.bat
@@ -12,9 +12,9 @@ del ..\Artifacts\lib3mf\_githash_win64_lib3mf.txt
 
 cmake ..\submodules\lib3mf
 cmake --build . --config Release
-copy Release\lib3mf.dll ..\..\Libraries\lib3mf\lib3mf_win64.dll /Y
+copy Release\lib3mf.dll ..\Artifacts\lib3mf\lib3mf_win64.dll /Y
 
-git rev-parse --verify HEAD >"..\..\Artifacts\lib3mf\_githash_win64_lib3mf.txt"
+git rev-parse --verify HEAD >"..\Artifacts\lib3mf\_githash_win64_lib3mf.txt"
 
 cd ..
 
