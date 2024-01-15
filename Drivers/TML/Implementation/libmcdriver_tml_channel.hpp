d3a1ebf0570047f94e10ec42afd5d40c388a8fee
Alexander Oster <alexanderoster@users.noreply.github.com>
2024-01-15 11:32:17 +0100

    Added first Technosoft Driver

---
+++ Drivers/TML/Implementation/libmcdriver_tml_channel.hpp
@@ -0,0 +1,77 @@
+/*++
+
+Copyright (C) 2024 Autodesk Inc.
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the Autodesk Inc. nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL AUTODESK INC. BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+Abstract: This is the class declaration of CChannel
+
+*/
+
+
+#ifndef __LIBMCDRIVER_TML_CHANNEL
+#define __LIBMCDRIVER_TML_CHANNEL
+
+#include "libmcdriver_tml_interfaces.hpp"
+
+// Parent classes
+#include "libmcdriver_tml_base.hpp"
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable : 4250)
+#endif
+
+// Include custom headers here.
+
+
+namespace LibMCDriver_TML {
+namespace Impl {
+
+
+/*************************************************************************************************************************
+ Class declaration of CChannel 
+**************************************************************************************************************************/
+
+class CChannel : public virtual IChannel, public virtual CBase {
+private:
+
+
+public:
+
+
+	IAxis * SetupAxis(const std::string & sIdentifier, const LibMCDriver_TML_uint32 nIndexInSetup) override;
+
+	void Close() override;
+
+};
+
+} // namespace Impl
+} // namespace LibMCDriver_TML
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
+#endif // __LIBMCDRIVER_TML_CHANNEL
