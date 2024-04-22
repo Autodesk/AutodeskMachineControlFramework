aae08149f28da70b91d5c89ce555c156baacc30e
Alexander Oster <alexanderoster@users.noreply.github.com>
2024-04-22 18:37:31 +0200

    Linux fixes

--- Implementation/LibMCEnv/libmcenv_datatable.cpp
+++ Implementation/LibMCEnv/libmcenv_datatable.cpp
@@ -37,6 +37,7 @@ Abstract: This is a stub class definition of CDataTable
 // Include custom headers here.
 #include "common_utils.hpp"
 #include <vector>
+#include <algorithm>
 #include <sstream>
 
 #include "amc_constants.hpp"
