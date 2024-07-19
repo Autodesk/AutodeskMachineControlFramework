2b69de14ef4a05e7ffc47e06718198ef2cc5110e
Alexander Oster <alexander.oster@autodesk.com>
2024-07-19 14:57:17 +0200

    Fixed Lib3MF Build script

--- Artifacts/build_lib3mf_win64.bat
+++ Artifacts/build_lib3mf_win64.bat
@@ -11,7 +11,7 @@ del ..\Artifacts\lib3mf\lib3mf_win64.dll
 del ..\Artifacts\lib3mf\_githash_lib3mf.txt
 
 cmake ..\submodules\lib3mf -DLIB3MF_TESTS=OFF
-cmake -DLIB3MF_TESTS=OFF  --build . --config Release
+cmake --build . --config Release
 copy Release\lib3mf.dll ..\Artifacts\lib3mf\lib3mf_win64.dll /Y
 
 cd ..\submodules\lib3mf
