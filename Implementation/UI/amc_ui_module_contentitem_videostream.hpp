199f7e732ee1b50b7174c68dbfb9020b786d008c
Alexander Oster <alexanderoster@users.noreply.github.com>
2026-02-10 18:00:16 +0100

    Added new Videostream UI widget

---
+++ Implementation/UI/amc_ui_module_contentitem_videostream.hpp
@@ -0,0 +1,82 @@
+/*++
+
+Copyright (C) 2020 Autodesk Inc.
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+	* Redistributions of source code must retain the above copyright
+	  notice, this list of conditions and the following disclaimer.
+	* Redistributions in binary form must reproduce the above copyright
+	  notice, this list of conditions and the following disclaimer in the
+	  documentation and/or other materials provided with the distribution.
+	* Neither the name of the Autodesk Inc. nor the
+	  names of its contributors may be used to endorse or promote products
+	  derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL AUTODESK INC. BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+
+#ifndef __AMC_UI_MODULE_CONTENTITEM_VIDEOSTREAM
+#define __AMC_UI_MODULE_CONTENTITEM_VIDEOSTREAM
+
+#include "header_protection.hpp"
+
+#ifndef __AMCIMPL_UI_MODULE
+#error this header is protected and should only be included in the corresponding implementation CPP files.
+#endif
+
+#include "amc_ui_module_contentitem.hpp"
+#include "pugixml.hpp"
+#include "amc_ui_expression.hpp"
+#include "amc_statemachinedata.hpp"
+
+
+namespace AMC {
+
+	amcDeclareDependingClass(CUIModule_ContentVideoStream, PUIModule_ContentVideoStream);
+	amcDeclareDependingClass(CUIModuleEnvironment, PUIModuleEnvironment);
+
+	class CUIModule_ContentVideoStream : public CUIModule_ContentItem {
+	protected:		
+		CUIExpression m_StreamResource;
+		CUIExpression m_AspectRatio;
+		CUIExpression m_MaxWidth;
+		CUIExpression m_MaxHeight;
+
+		PStateMachineData m_pStateMachineData;
+
+	public:
+
+		static PUIModule_ContentVideoStream makeFromXML(const pugi::xml_node& xmlNode, const std::string& sItemName, const std::string& sModulePath, PUIModuleEnvironment pUIModuleEnvironment);
+
+		CUIModule_ContentVideoStream(const std::string & sUUID, const std::string& sItemName, const std::string& sModulePath, CUIExpression streamResource, CUIExpression dAspectRatio, CUIExpression maxWidth, CUIExpression maxHeight, PStateMachineData pStateMachineData);
+		
+		virtual ~CUIModule_ContentVideoStream();
+
+		void addLegacyContentToJSON(CJSONWriter& writer, CJSONWriterObject& object, CParameterHandler* pClientVariableHandler, uint32_t nStateID) override;
+
+		virtual void configurePostLoading() override;
+
+		virtual void populateClientVariables(CParameterHandler* pClientVariableHandler) override;
+
+		virtual std::string findElementPathByUUID(const std::string& sUUID) override;
+	};
+
+
+}
+
+
+#endif //__AMC_UI_MODULE_CONTENTITEM_VIDEOSTREAM
